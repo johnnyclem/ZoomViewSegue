fbfa8f4bea1e8b058b9e8c5c8e3f4bc8e2d7e3fd
Bennett Smith <bennett@focalshift.com>
2013-03-19 18:55:38 -0700

    Improves animation.

--- ZoomViewSegue/ZoomToTileSegue.m
+++ ZoomViewSegue/ZoomToTileSegue.m
@@ -13,36 +13,67 @@
 
 - (void)perform
 {
+    NSLog(@"[%@ %@]", [self class], NSStringFromSelector(_cmd));
+    
+    // Get the view controllers.
     UIViewController *svc = (UIViewController *) self.sourceViewController;
     UIViewController *dvc = (UIViewController *) self.destinationViewController;
     
-    UIImage *sourceViewImage = [UIImage screenshotFromView:svc.view];
-    UIImageView *tempView = [[UIImageView alloc] initWithImage:sourceViewImage];
+    // Grab a screen shot.
+    UIImage *screenshot = [UIImage screenshotFromView:svc.view.window];
     
-    [svc.navigationController pushViewController:dvc animated:NO];
-
-    UIView *actualDestinationView = dvc.view;
-    dvc.view = tempView;
-
-    CGRect frame = actualDestinationView.frame;
-    frame.origin.x = 0.0f;
-    frame.origin.y = 0.0f;
-    actualDestinationView.frame = frame;
+    // Create a UIImageView using the screen shot.
+    UIImageView *screenshotView = [[UIImageView alloc] initWithImage:screenshot];
     
-    [actualDestinationView setTransform:CGAffineTransformMakeScale(0.25f, 0.25f)];
-    [actualDestinationView setAlpha:0.0];
+    // Convert from window coordinates to view coordinates and update frame.
+    CGRect screenshotFrame = [svc.view convertRect:screenshotView.frame fromView:svc.view.window];
+    screenshotView.frame = screenshotFrame;
     
-    [dvc.view addSubview:actualDestinationView];
+    // Don't clip subviews
+    [svc.view setClipsToBounds:NO];
+    
+    // Add the screenshot view.
+    [svc.view addSubview:screenshotView];
+    
+    // Add the destination view.
+    [svc.view addSubview:dvc.view];
+    
+    // Make sure the other views are in front of the navigation bar.
+    [svc.navigationController.navigationBar.superview sendSubviewToBack:svc.navigationController.navigationBar];
+    
+    // Convert from window coordinates to view coordinates and update frame.
+    CGRect destinationViewFrame = [svc.view convertRect:dvc.view.frame fromView:svc.view.window];
+    dvc.view.frame = destinationViewFrame;
+    
+    // Start with the destination view at 1/4 scale and completely transparent.
+    [dvc.view setTransform:CGAffineTransformMakeScale(0.25f, 0.25f)];
+    [dvc.view setAlpha:0.0];
     
     [UIView animateWithDuration:0.1667f
                           delay:0.0f
                         options:UIViewAnimationOptionCurveEaseOut
                      animations:^{
-                         [actualDestinationView setTransform:CGAffineTransformMakeScale(1.0f, 1.0f)];
-                         [actualDestinationView setAlpha:1.0];
+                         [dvc.view setTransform:CGAffineTransformMakeScale(1.0f, 1.0f)];
+                         [dvc.view setAlpha:1.0];
                      }
                      completion:^(BOOL finished){
-                         dvc.view = actualDestinationView;
+                         
+                         // No longer need the screenshot view.
+                         [screenshotView removeFromSuperview];
+                         
+                         // Destination view should no longer be part of the source
+                         // view hierarchy so remove it.
+                         [dvc.view removeFromSuperview];
+                         
+                         // Turn off the navigation bar.
+                         [svc.navigationController setNavigationBarHidden:YES animated:NO];
+                         
+                         // Move the navigation bar back in front of other views.
+                         [svc.navigationController.navigationBar.superview
+                          bringSubviewToFront:svc.navigationController.navigationBar];
+                         
+                         // Push to the destination view controller.
+                         [svc.navigationController pushViewController:dvc animated:NO];
                      }];
 }
 
